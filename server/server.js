bf7b4f084edc15b5d034020da6857d31432a71a6
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2025-10-10 01:43:11 -0400

    Add Vercel URL to CORS

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 const corsOptions = {
   origin: [
     'http://localhost:5173', 
-    'http://localhost:5000',
+    'http://localhost:5001',
     'https://inventory-tracker-frontend-ten.vercel.app'
   ], 
   credentials: true,
